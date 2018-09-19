2b75da73c130d6f1a4ac1539986002c3ca2a4ffa
SALASdavidELECTRONICS20141996 <@davidSalasDevelop>
2018-09-19 14:46:29 -0600

    cmabios subiendo

---
+++ TiendaReactWebNextU-master/server/datos/articuloModel.js
@@ -0,0 +1,13 @@
+/**
+ * @author David Salas
+ */
+var mongoose = require('mongoose');
+	
+var articuloSchema = new mongoose.Schema({
+	nombre: {type: String, required: true},
+	img: {type: String, required: true},
+	precio: {type: String, required: true},
+	stock: String,
+});
+var Articulo = mongoose.model('Articulo', articuloSchema);
+module.exports = Articulo;
